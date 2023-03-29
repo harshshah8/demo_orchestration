ae02b23309f14ab69624941b3d43fb1ba571f4e8
parul <parulghotikar@gmail.com>
2023-03-29 23:26:10 +0530

    added the sample api

--- src/main/java/demo/orchestration/controller/OrchController.java
+++ src/main/java/demo/orchestration/controller/OrchController.java
@@ -1,5 +1,19 @@
 package demo.orchestration.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
 public class OrchController {
+
+    @GetMapping("")
+    public HttpStatus health()
+    {
+        return (HttpStatus.OK);
+    }
     
 }

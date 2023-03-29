f467fe6c272b467a10a35d8f344cb618b7793a23
parul <parulghotikar@gmail.com>
2023-03-29 23:26:49 +0530

    added the health api

--- src/main/java/demo/orchestration/controller/OrchController.java
+++ src/main/java/demo/orchestration/controller/OrchController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/search")
 public class OrchController {
 
-    @GetMapping("")
+    @GetMapping("/health")
     public HttpStatus health()
     {
         return (HttpStatus.OK);

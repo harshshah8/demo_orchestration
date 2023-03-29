eaf62884e595d9f8a7a762fe06997d112e0f4471
parul <parulghotikar@gmail.com>
2023-03-30 00:00:31 +0530

    added config file

---
+++ src/main/java/demo/orchestration/config/MyConfig.java
@@ -0,0 +1,5 @@
+package demo.orchestration.config;
+
+public class MyConfig {
+    
+}
--- src/main/java/demo/orchestration/controller/OrchController.java
+++ src/main/java/demo/orchestration/controller/OrchController.java
@@ -2,8 +2,12 @@ package demo.orchestration.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import demo.orchestration.dto.RequestDto;
 
 @RestController
 @RequestMapping("/search")
@@ -13,6 +17,12 @@ public class OrchController {
     public HttpStatus health()
     {
         return (HttpStatus.OK);
+    }
+
+    @PostMapping("/check")
+    public HttpStatus check(@RequestBody RequestDto requestDto)
+    {
+        return(HttpStatus.OK);
     }
     
 }
